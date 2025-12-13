0f31f2cd1106863a675bc4eed30abd58dc01d335
Teemu671 <willniko67@gmail.com>
2025-12-13 22:05:04 +0200

    Complete

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function menu() {
     x.style.display = "block";
   }
 }
+function cart() {
+  var y = document.getElementById("content");
+  var x = document.getElementById("cart");
+  if (x.style.display === "block") {
+    y.style.display = "block";
+    x.style.display = "none";
+  } else {
+    y.style.display = "none";
+    x.style.display = "block";
+  }
+}
+function inc() {
+  alert("Added to cart")
+  
+}
+function dec() {
+  alert("Removed from cart")
+  
+}
+function nut(e) {
+  var x = document.getElementById("nut"+e);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
